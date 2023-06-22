27526b8105f0be638947a323643e36d4456cdf19
Timothy Ford <71365012+tsfordcoding@users.noreply.github.com>
2023-06-21 17:40:01 -0700

    first commit

--- src/app.js
+++ src/app.js
@@ -4,10 +4,31 @@ const app = express();
 
 app.use(morgan("dev"));
 
-const sayHello = (req, res, next) => {
-    res.send("Hello");
+// saySomething callback function
+const saySomething = (req, res) => {
+    const greeting = req.params.greeting;
+    const name = req.query.name;
+
+    const content = greeting && name ? `${greeting}, ${name}!` : `${greeting}!`;
+    res.send(content);
 };
 
+// sayHello callback function
+const sayHello = (req, res) => {
+    console.log(req.query);
+    const name = req.query.name;
+    const content = name ? `Hello, ${name}!` : `Hello!`;
+    res.send(content);
+};
+
+// sayGoodbye callback function
+const sayGoodbye = (req, res) => {
+    res.send("Sorry to see you go!");
+};
+
+// Routes
+app.get("/say/goodbye", sayGoodbye);
+app.get("/say/:greeting", saySomething);
 app.get("/hello", sayHello);
 
 module.exports = app;
